cff1ad5afcc35fd95a7830a6ae4e6a2ebaaf831a
Vlas Sokolov <vlas145@gmail.com>
2015-12-10 13:24:55 -0500

    Restructured, the template is now more flexible.

--- map_template.py
+++ map_template.py
@@ -5,6 +5,11 @@ import sys
 import os
 from astropy.io import fits
 
+# TODO: restucture - make a __main__ block with following arguments:
+#	- cloud
+#	- do_sdfits
+#	- do_calibration
+#	- do_imaging
 cloud = 'A'
 from cloud_keys import keys
 session = keys[cloud]['session'] 
@@ -16,6 +21,14 @@ scans   = keys[cloud]['scans'  ]
 gains   = keys[cloud]['gains'  ] 
 windows = ['2'] # Only NHc(1,1) to test the system
 
+# Re-generate sdfits files
+import subprocess
+assert type(scans) is list # better safe than sorry
+scan_blocks = ','.join(['%i:%i'%(s['start'],s['end']) for s in scans])
+sdfits_args = '-scans=\"%s\" %.2i' % (scan_blocks, session)
+subprocess.Popen('sdfits-test -backends=vegasi AGBT15B_313_'+sdfits_args)
+
+# Run the GAS pipeline wrapper
 for window in windows:
 	for block in range(nblocks):	
 		GAS_gridregion.doPipeline(
@@ -32,14 +45,15 @@ for window in windows:
 #mv *window0*.fits cloudA_NH3_11/.
 # TODO: a proper handling of directory struture
 
-data_dir='/lustre/pipeline/scratch/vsokolov/GBT-15B-313/'
+# Image the calibrated data
 #data_dir='/lustre/pipeline/scratch/jpineda/Vlas/GBT-15B-313/'
+data_dir='/lustre/pipeline/scratch/vsokolov/GBT-15B-313/'
 startChannel, endChannel = 3200, 5000
-
 file_extension='_test'
-GAS_gridregion.griddata( rootdir=data_dir, region=Region, 
-        dirname=Region+'_NH3_11', 
-        startChannel = startChannel, endChannel = endChannel, 
-        file_extension=file_extension,
-	#baselineRegion = [TODO]
-	)
+GAS_gridregion.griddata(rootdir=data_dir, 
+                        region=Region, 
+                        dirname=Region+'_NH3_11', 
+                        startChannel = startChannel, 
+                        endChannel = endChannel, 
+                        #baselineRegion = [TODO],
+                        file_extension=file_extension)
