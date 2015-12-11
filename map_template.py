a983d8cd5d8702d5e3b64258b0f166922e5ef72f
Vlas Sokolov <vlas145@gmail.com>
2015-12-10 23:59:33 -0500

    First working version.

--- map_template.py
+++ map_template.py
@@ -21,10 +21,10 @@ def main():
 	parser = argparse.ArgumentParser(description =
 	                    'Runs the GAS pipeline on GBT-15B-313.')
 	parser.add_argument('--sdfits', dest='do_sdfits', 
-			    action='store_false', default=False, 
+			    action='store_true', default=False, 
 			    help='re-run sdfits on VEGAS data')
 	parser.add_argument('--calib', dest='do_calibration', 
-			   action='store_false', default=False, 
+			   action='store_true', default=False, 
 			   help='run gbtpipeline GAS wrapper?')
 	parser.add_argument('--noimage', dest='no_imaging', 
 			    action='store_true', default=False, 
@@ -41,9 +41,8 @@ def main():
 		print 'consider passing --source X Y Z',
 		print 'from the command line'
 		args.source = ['A','B','E','I','J']
-
 	for cloud in args.source:
-		map_cloud(cloud=cloud, do_calibraion=args.do_calibration,
+		map_cloud(cloud=cloud, do_calibration=args.do_calibration,
 		          do_sdfits=args.do_sdfits, 
 		          do_imaging=not args.no_imaging)
 
@@ -55,8 +54,8 @@ def map_cloud(cloud, do_sdfits=False, do_calibration=False, do_imaging=True, key
 	nblocks = keys[cloud]['nblocks']
 	scans   = keys[cloud]['scans'  ] 
 	gains   = keys[cloud]['gains'  ] 
-	windows = ['2'] # Only NHc(1,1) to test the system
-	
+	windows = ['2','3'] # Only NHc(1,1) to test the system
+
 	# Convert VEGAS raw data to sdfits
 	if do_sdfits:
 		import subprocess
@@ -65,14 +64,19 @@ def map_cloud(cloud, do_sdfits=False, do_calibration=False, do_imaging=True, key
 				       for s in scans])
 		sdfits_dir = ' AGBT15B_313_%.2i' % session
 		sdfits_args = ' -scans=\"%s\"' % scan_blocks
+		# TODO: oops I also need to properly set the output dir!
 		subprocess.Popen('sdfits-test -backends=vegasi'+
 				 sdfits_args+sdfits_dir)
 
 	# it's being quite slow on the import, moved inside the script	
 	import GAS_gridregion
+	data_dir='/lustre/pipeline/scratch/vsokolov/'
 	# Run the GAS pipeline wrapper
 	if do_calibration:
 		for window in windows:
+			# TODO: this is way too slow; gbtpipeline can accept
+			# arguments like -m "50:60,80:90", rewrite the 
+			# wrapper to accept faster arguments
 			for block in range(nblocks):	
 				GAS_gridregion.doPipeline(
 				   SessionNumber = session,
@@ -82,23 +86,20 @@ def map_cloud(cloud, do_sdfits=False, do_calibration=False, do_imaging=True, key
 				   Gains         = gains, 
 				   Region        = region, 
 				   Window        = str(window), 
-				   overwrite     =True                    )
-	
-	# I had to run this manually :S
-	#mv *window0*.fits cloudA_NH3_11/.
-	# TODO: a proper handling of directory struture
+				   OutputRoot    = data_dir+region,
+				   overwrite     = True                   )
 	
 	# Image the calibrated data
-	#data_dir='/lustre/pipeline/scratch/jpineda/Vlas/GBT-15B-313/'
-	startChannel, endChannel = 3200, 5000
-	file_extension='_test'
-	GAS_gridregion.griddata(rootdir=data_dir, 
-	                        region=Region, 
-	                        dirname=Region+'_NH3_11', 
-	                        startChannel = startChannel, 
-	                        endChannel = endChannel, 
-	                        #baselineRegion = [TODO],
-	                        file_extension=file_extension)
+	if do_imaging:
+		startChannel, endChannel = 3200, 5000
+		file_extension='_test'
+		GAS_gridregion.griddata(rootdir=data_dir, 
+		                        region=region, 
+		                        dirname=region+'_NH3_11', 
+		                        startChannel = startChannel, 
+		                        endChannel = endChannel, 
+		                        #baselineRegion = [TODO],
+		                        file_extension=file_extension)
 
 if __name__ == "__main__":
 	main()
